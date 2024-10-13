d38a9ea28edc164c595742c0a582fd6f7fb2cfdf
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2024-10-13 02:31:26 +0100

    fix: make default color black

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const PROTOCOL = "1.0";
 
-export const BRANDCOLOR = localStorage.getItem("theme-color") || "#0582D2";
+export const BRANDCOLOR = localStorage.getItem("theme-color") || "#00000";
 
 export const CURRENCYPAIRS = [
   {
